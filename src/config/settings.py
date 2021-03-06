5f33b193148f9d63190128e8ffe97397213bd365
Oke Agi-Tuedor <mail2oke@gmail.com>
2021-03-06 10:47:57 +0100

    install pymsql

--- src/config/settings.py
+++ src/config/settings.py
@@ -9,9 +9,16 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
+
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+
+import pymysql
+
+pymysql.install_as_MySQLdb()
+pymysql.version_info = (1, 4, 0, "final", 0)
+
 
 load_dotenv()
 
