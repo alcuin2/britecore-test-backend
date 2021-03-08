b91815357f658daf6b358cf83a343f0b1e75d007
Oke Agi-Tuedor <mail2oke@gmail.com>
2021-03-08 10:24:13 +0100

    settings to false

--- src/config/settings.py
+++ src/config/settings.py
@@ -33,7 +33,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "z&7@k6)!6f&4-dl$p!7s-(t==e3zx6cqy90lmlyjx#6@29!a@@"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
